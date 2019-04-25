e9bd6cf7df024f42e8069e11e41dc1bfd95a8a40
Ramesh Gummadi <rameshgummadi@gmail.com>
2019-04-25 10:06:11 +0530

    msg

---
+++ proman-service/src/main/java/com/upgrad/proman/service/dao/UserDao.java
@@ -0,0 +1,29 @@
+package com.upgrad.proman.service.dao;
+
+import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity) {
+        entityManager.persist(userEntity);
+        return userEntity;
+    }
+
+    public UserEntity getUser(final String userUuid) {
+        try {
+            return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", userUuid).getSingleResult();
+        } catch(NoResultException nre) {
+            return null;
+        }
+
+        }
+}
